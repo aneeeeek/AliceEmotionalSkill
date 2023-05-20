f2e3534e1a47822afd1bffd4f99179c9a9ddd028
Ann Kuznetsova <anniekuznec@mail.ru>
2023-05-20 19:09:25 +0300

    Добавление и обновление сессии

--- dbConnection.py
+++ dbConnection.py
@@ -47,3 +47,211 @@ def getQuestion(id):
                 return result
     except Error as e:
         print(e)
+
+def getUsedQuestionIDs(id_session):
+    try:
+        with connect(
+                host="localhost",
+                user="root",
+                password="anek",
+                database="aliceskill",
+        ) as connection:
+            query = "SELECT id_question FROM aliceskill.sessionstoquestions WHERE id_session='"+str(id_session)+"';"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                result = cursor.fetchall()
+                return result
+    except Error as e:
+        print(e)
+
+def getQuestionType(id):
+    try:
+        with connect(
+                host="localhost",
+                user="root",
+                password="anek",
+                database="aliceskill",
+        ) as connection:
+            query = "SELECT id_type FROM aliceskill.questions WHERE id_question="+str(id)+";"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                result = cursor.fetchall()
+                return result
+    except Error as e:
+        print(e)
+
+def getUsers():
+    try:
+        with connect(
+                host="localhost",
+                user="root",
+                password="anek",
+                database="aliceskill",
+        ) as connection:
+            query = "SELECT id_user FROM aliceskill.users;"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                result = cursor.fetchall()
+                return result
+    except Error as e:
+        print(e)
+
+def addUser(id_user):
+    try:
+        with connect(
+                host="localhost",
+                user="root",
+                password="anek",
+                database="aliceskill",
+        ) as connection:
+            query = "INSERT into aliceskill.users (id_user) VALUES ('"+str(id_user)+"');"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                connection.commit()
+    except Error as e:
+        print(e)
+
+def addResult(good,neutral,bad,anx,fr,aggr,rig):
+    try:
+        with connect(
+                host="localhost",
+                user="root",
+                password="anek",
+                database="aliceskill",
+        ) as connection:
+            query = "INSERT into aliceskill.results (goodScore, neutralScore, badScore, anxiety, frustration," \
+                    "aggressiveness, rigidity) " \
+                    "VALUES ("+str(good)+","+str(neutral)+","+str(bad)+","+str(anx)+","+str(fr)+","+str(aggr)+","+str(rig)+");"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                connection.commit()
+    except Error as e:
+        print(e)
+
+def updateResult(id_res,good,neutral,bad,anx,fr,aggr,rig):
+    try:
+        with connect(
+                host="localhost",
+                user="root",
+                password="anek",
+                database="aliceskill",
+        ) as connection:
+            query = "UPDATE aliceskill.results SET goodScore="+str(good)+",neutralScore="+str(neutral)+\
+                    ",badScore="+str(bad)+",anxiety="+str(anx)+",frustration="+str(fr)+\
+                    ",aggressiveness="+str(aggr)+", rigidity="+str(rig)+" WHERE id_result="+str(id_res)+";"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                connection.commit()
+    except Error as e:
+        print(e)
+
+def getResult(id_session):
+    try:
+        with connect(
+                host="localhost",
+                user="root",
+                password="anek",
+                database="aliceskill",
+        ) as connection:
+            query = "SELECT id_result FROM aliceskill.sessions WHERE id_session='"+id_session+"';"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                result = cursor.fetchall()
+                return result
+    except Error as e:
+        print(e)
+
+def getLastResult():
+    try:
+        with connect(
+                host="localhost",
+                user="root",
+                password="anek",
+                database="aliceskill",
+        ) as connection:
+            query = "SELECT id_result FROM aliceskill.results ORDER BY id_result DESC LIMIT 1;"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                result = cursor.fetchall()
+                return result
+    except Error as e:
+        print(e)
+
+def addSession(id_session, new, datetime, id_user, id_result, isFinished, id_lastQuestion, answerNumber):
+    try:
+        with connect(
+                host="localhost",
+                user="root",
+                password="anek",
+                database="aliceskill",
+        ) as connection:
+            query = "INSERT into aliceskill.sessions (id_session, new, dateTime, id_user, id_result,isFinished, id_lastQuestion, answerNumber) " \
+                    "VALUES ('"+str(id_session)+"',"+str(new)+",'"+str(datetime)+"','"+str(id_user)+"',"+str(id_result)+","+str(isFinished)+","+str(id_lastQuestion)+","+str(answerNumber)+");"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                connection.commit()
+    except Error as e:
+        print(e)
+
+def updateSession(id_session, new, isFinished,answerNumber):
+    try:
+        with connect(
+                host="localhost",
+                user="root",
+                password="anek",
+                database="aliceskill",
+        ) as connection:
+            query = "UPDATE aliceskill.sessions SET new="+str(new)+",isFinished="+str(isFinished)+",answerNumber="+str(answerNumber)+\
+                    " WHERE id_session='"+str(id_session)+"';"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                connection.commit()
+    except Error as e:
+        print(e)
+
+def addUsedQuestion(id_session,id_question):
+    try:
+        with connect(
+                host="localhost",
+                user="root",
+                password="anek",
+                database="aliceskill",
+        ) as connection:
+            query = "INSERT into aliceskill.sessionstoquestions (id_session,id_question) VALUES ('"+str(id_session)+"',"+str(id_question)+");"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                connection.commit()
+    except Error as e:
+        print(e)
+
+def updateLastQuestionID(session,id):
+    try:
+        with connect(
+                host="localhost",
+                user="root",
+                password="anek",
+                database="aliceskill",
+        ) as connection:
+            query = "UPDATE aliceskill.sessions SET id_lastQuestion="+str(id)+\
+                    " WHERE id_session='"+str(session)+"';"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                connection.commit()
+    except Error as e:
+        print(e)
+
+def getLastQuestion(session):
+    try:
+        with connect(
+                host="localhost",
+                user="root",
+                password="anek",
+                database="aliceskill",
+        ) as connection:
+            query = "SELECT id_lastQuestion FROM aliceskill.sessions WHERE id_session='"+str(session)+"';"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                result = cursor.fetchall()
+                return result
+    except Error as e:
+        print(e)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 import random
 from flask import Flask, request
 import answers
@@ -12,17 +13,24 @@ answerNumber = 0
 app = Flask(__name__)
 
 goodScore, neutralScore, badScore = 0, 0, 0
-usedIDs = []
+anxiety, frustration, aggressiveness, rigidity = 0,0,0,0
+session = 0
 
 @app.route("/alice", methods=["POST"])
 def main():
     #todo update session
+    global session
     global answerNumber
-    text = request.json.get('request', {}).get('original_utterance')
 
-    # todo проверять, тот ли пользователь проходит тест
+    session = request.json.get('session', {}).get('session_id')
+    if request.json.get('session', {}).get('new'):
+        addSession(session)
+
+
+    text = request.json.get('request', {}).get('original_utterance').lower()
 
     if isExitText(text):
+        updateSession(session, 0, 1, answerNumber)
         return response("До встречи!", True)
 
     # диалог с пользователем начался
@@ -31,84 +39,117 @@ def main():
 
     # диалог с пользователем продолжается
     else:
-
-        # ответ на вопрос о прохождении навыка
+        # ответ на вопрос о прохождении навыка, вопрос 1
         if answerNumber == 0:
-            if text.lower() == 'давай':
+            if text == 'давай':
                 answerNumber += 1
-                return response(getTextQuestion(), False)
+                text = getTextQuestion()
+                updateSession(session, 0, 0, answerNumber)
+                return response(text, False)
             else:
                 return response("Ну и пока", True)
 
-        # ответ на текстовый вопрос 1
+        # ответ на текстовый вопрос 1, вопрос 2
         elif answerNumber == 1:
-            #addScoreForText(getSentiment(text))
+            addScore(text)
             answerNumber += 1
             return response(getBinaryQuestion(), False)
 
-        # ответ на вопрос да/нет/возможно 1
+        # ответ на вопрос да/нет 1, вопрос 3
         elif answerNumber == 2:
-            if text == 'да' or text=='нет':
-                #addScoreForYesNo(text)
+            isAdded = addScore(text)
+            if isAdded == 0:
+                return response("Ответьте только словом да или нет", False)
+            else:
                 answerNumber += 1
+                updateSession(session, 0, 0, answerNumber)
                 return response(getRatingQuestion(), False)
-            else:
-                return response("Ответьте словами ДА или НЕТ", False)
 
-        # ответ на вопрос числом 1
+        # ответ на вопрос числом 1, вопрос 4
         elif answerNumber == 3:
-            if text.isnumeric():
-                if 5 >= int(text) >= 1:
-                    addScoreForNumeric(int(text))
-                    answerNumber += 1
-                    return response(getTextQuestion(), False)
+            isAdded = addScore(text)
+            if isAdded == 0:
+                return response("Ответьте только числом от 1 до 5", False)
             else:
-                return response("Ответьте целым числом в заданном диапазоне: от 1 до 5", False)
+                answerNumber += 1
+                updateSession(session, 0, 0, answerNumber)
+                return response(getTextQuestion(), False)
 
-        # ответ на текстовый вопрос 2
+        # ответ на текстовый вопрос 2, вопрос 5
         elif answerNumber == 4:
-            addScoreForText(getSentiment(text))
-            getSentiment(text)
+            addScore(text)
             answerNumber += 1
-            return response(getRatingQuestion(), False)
-
-        # ответ на вопрос числом 2
-        elif answerNumber == 5:
-            if text.isnumeric():
-                if 5 >= int(text) >= 1:
-                    addScoreForNumeric(int(text))
-                    answerNumber += 1
-                    # TODO посмотреть на скоры и в зависимости от них задать следующий вопрос
-                    return response(getBinaryQuestion(), False)
-            else:
-                return response("Ответьте целым числом в заданном диапазоне: от 1 до 5", False)
-
-        # ответ на вопрос да/нет/возможно 2
-        elif answerNumber == 6:
-            # TODO проверить что сказал то что нужно - если нет, попросить повторить ответ в заданной форме
-            answerNumber += 1
-            # TODO посмотреть на скоры и в зависимости от них задать следующий вопрос
+            updateSession(session, 0, 0, answerNumber)
             return response(getBinaryQuestion(), False)
 
-        # ответ на вопрос да/нет/возможно 3
-        else:
-            # TODO проверить что сказал то что нужно - если нет, попросить повторить ответ в заданной форме
-
-
-            result = getMaxScore(goodScore, neutralScore, badScore)
-            allScore = "На основе анализа вашего настроения " #"Вы набрали " + str(goodScore) + " положительных баллов, " + str(neutralScore) + " нейтральных баллов, " + str(badScore) + " негативных баллов, "
-            if result == "goodScore":
-                return response(
-                    allScore + "можно сделать вывод, что Вы чувствуете себя хорошо! Сохраняйте позитивный настрой!",
-                    True)  # закончить сессию
-            elif result == "neutralScore":
-                return response(
-                    allScore + "можно сделать вывод, что Вы чувствуете себя не так уж плохо! Все наладится, главное оставаться сильным!",
-                    True)  # закончить сессию
+        # ответ на вопрос да/нет 2, вопрос 6
+        elif answerNumber == 5:
+            isAdded = addScore(text)
+            if isAdded == 0:
+                return response("Ответьте только словом да или нет", False)
             else:
-                return response(
-                    allScore + "можно сделать вывод, что Вы чувствуете себя плохо! Не бойтесь, черные полосы всегда проходят!",
-                    True)  # закончить сессию
+                answerNumber += 1
+                updateSession(session, 0, 0, answerNumber)
+                return response(getRatingQuestion(), False)
+
+        # ответ на вопрос числом 2, вопрос 7
+        elif answerNumber == 6:
+            isAdded = addScore(text)
+            if isAdded == 0:
+                return response("Ответьте только числом от 1 до 5", False)
+            else:
+                answerNumber += 1
+                updateSession(session, 0, 0, answerNumber)
+                return response(getTextQuestion(), False)
+
+        # ответ на вопрос текстом 3, вопрос 8
+        elif answerNumber == 7:
+            addScore(text)
+            answerNumber += 1
+            updateSession(session, 0, 0, answerNumber)
+            return response(getBinaryQuestion(), False)
+
+        # ответ на вопрос да/нет 3, вопрос 9
+        elif answerNumber == 8:
+            isAdded = addScore(text)
+            if isAdded == 0:
+                return response("Ответьте только словом да или нет", False)
+            else:
+                answerNumber += 1
+                updateSession(session, 0, 0, answerNumber)
+                return response(getRatingQuestion(), False)
+
+        # ответ на вопрос числом 3, вопрос 10
+        elif answerNumber == 9:
+            isAdded = addScore(text)
+            if isAdded == 0:
+                return response("Ответьте только числом от 1 до 5", False)
+            else:
+                answerNumber += 1
+                updateSession(session, 0, 0, answerNumber)
+                return response(getRatingQuestion(), False)
+
+        # ответ на вопрос числом 4, результат
+        else:
+            isAdded = addScore(text)
+            if isAdded == 0:
+                return response("Ответьте только числом от 1 до 5", False)
+            else:
+                updateSession(session, 0, 1, answerNumber)
+                result = getMaxScore(goodScore, neutralScore, badScore)
+                allScore = "На основе анализа вашего настроения " #"Вы набрали " + str(goodScore) + " положительных баллов, " + str(neutralScore) + " нейтральных баллов, " + str(badScore) + " негативных баллов, "
+                if result == "goodScore":
+                    return response(
+                        allScore + "можно сделать вывод, что Вы чувствуете себя хорошо! Сохраняйте позитивный настрой!",
+                        True)  # закончить сессию
+                elif result == "neutralScore":
+                    return response(
+                        allScore + "можно сделать вывод, что Вы чувствуете себя не так уж плохо! Все наладится, главное оставаться сильным!",
+                        True)  # закончить сессию
+                else:
+                    return response(
+                        allScore + "можно сделать вывод, что Вы чувствуете себя плохо! Не бойтесь, черные полосы всегда проходят!",
+                        True)  # закончить сессию
 
 
 # === Получить текстовый вопрос, который ранее не задавался пользователю ===
@@ -133,7 +174,7 @@ def getBinaryQuestion():
 
     return returnQuestionText(idList)
 
-# === Получить текстовый вопрос, который ранее не задавался пользователю ===
+# === Получить числовой вопрос, который ранее не задавался пользователю ===
 def getRatingQuestion():
     if answerNumber == 3:
         idList = dbConnection.getIDRatingAnxietyQuestions()
@@ -149,13 +190,136 @@ def getRatingQuestion():
 def returnQuestionText(idList):
     # Выбрать случайный неповторяющийся индекс вопроса и записать его
     idRandom = random.choice(idList)
-    while idRandom in usedIDs:
-        idRandom = random.choice(idList)
-    usedIDs.append(idRandom)
+    usedIDs = dbConnection.getUsedQuestionIDs(session)
+    if usedIDs != None:
+        while idRandom in usedIDs:
+            idRandom = random.choice(idList)
+
+    dbConnection.addUsedQuestion(session,int(idRandom[0]))
+    dbConnection.updateLastQuestionID(session,int(idRandom[0]))
     # Получить текст вопроса по индексу
     textList = dbConnection.getQuestion(int(idRandom[0]))
     question = textList[0]
     return str(question[0])
+
+def addScore(answer):
+    global goodScore, neutralScore, badScore, anxiety, frustration, aggressiveness, rigidity
+
+    lastQ = dbConnection.getLastQuestion(session)
+    type = dbConnection.getQuestionType(int(lastQ[0][0]))
+    type = int(type[0][0])
+    if type == 1:
+        result = getSentiment(answer)
+        if result == 'positive':
+            goodScore += 1
+        elif result == 'neutral':
+            neutralScore += 1
+        elif result == 'negative':
+            badScore += 1
+        else:
+            neutralScore += 0.5
+    elif type == 2:
+        if answer == "да":
+            goodScore+=1
+        elif answer == "нет":
+            badScore+=1
+        else:
+            return 0
+    elif type == 3:
+        if answer == "да":
+            badScore+=1
+        elif answer == "нет":
+            goodScore+=1
+        else:
+            return 0
+    elif type == 4:
+        if answer.isnumeric():
+            if 5 >= int(answer) >= 1:
+                anxiety+=int(answer)
+                if int(answer) == 1:
+                    goodScore += 2
+                elif int(answer) == 2:
+                    goodScore += 1
+                elif int(answer) == 3:
+                    neutralScore+=1
+                elif int(answer) == 4:
+                    badScore += 1
+                else:
+                    badScore += 2
+            else: return 0
+        else: return 0
+    elif type == 5:
+        if answer.isnumeric():
+            if 5 >= int(answer) >= 1:
+                frustration+=int(answer)
+                if int(answer) == 1:
+                    goodScore += 2
+                elif int(answer) == 2:
+                    goodScore += 1
+                elif int(answer) == 3:
+                    neutralScore+=1
+                elif int(answer) == 4:
+                    badScore += 1
+                else:
+                    badScore += 2
+            else: return 0
+        else: return 0
+    elif type == 6:
+        if answer.isnumeric():
+            if 5 >= int(answer) >= 1:
+                aggressiveness+=int(answer)
+                if int(answer) == 1:
+                    goodScore += 2
+                elif int(answer) == 2:
+                    goodScore += 1
+                elif int(answer) == 3:
+                    neutralScore+=1
+                elif int(answer) == 4:
+                    badScore += 1
+                else:
+                    badScore += 2
+            else: return 0
+        else: return 0
+    else:
+        if answer.isnumeric():
+            if 5 >= int(answer) >= 1:
+                rigidity+=int(answer)
+                if int(answer) == 1:
+                    goodScore += 2
+                elif int(answer) == 2:
+                    goodScore += 1
+                elif int(answer) == 3:
+                    neutralScore+=1
+                elif int(answer) == 4:
+                    badScore += 1
+                else:
+                    badScore += 2
+            else: return 0
+        else: return 0
+
+    updateResult()
+
+def addSession(session):
+    user = request.json.get('session', {}).get('user',{}).get('user_id')
+    user_tuple=(user,)
+    userIDs = dbConnection.getUsers()
+    if user_tuple not in userIDs:
+        dbConnection.addUser(user)
+
+    dbConnection.addResult(0,0,0,0,0,0,0)
+    from datetime import datetime
+    date = datetime.now()
+    id_res = str(dbConnection.getLastResult()[0][0])
+    dbConnection.addSession(session, 1, date, user, id_res,0,0,0)
+
+
+def updateSession(session,isNew,isFinished,answerNumber):
+    dbConnection.updateSession(session,isNew,isFinished,answerNumber)
+
+def updateResult():
+    id_result = str(dbConnection.getResult(session)[0][0])
+    dbConnection.updateResult(id_result, goodScore, neutralScore, badScore,
+                              anxiety, frustration, aggressiveness, rigidity)
 
 def response(text, end_session):
     return {
@@ -197,30 +361,6 @@ def getSentiment(text):
     model = FastTextSocialNetworkModel(tokenizer=tokenizer)
     result = model.predict([text.lower()], k=1)
     return list(result[0].keys()).pop(0)
-
-
-def addScoreForText(result):
-    global goodScore, neutralScore, badScore
-    if result == 'positive':
-        goodScore += 1
-    elif result == 'neutral':
-        neutralScore += 1
-    elif result == 'negative':
-        badScore += 1
-    else:
-        neutralScore += 0.5
-
-
-def addScoreForNumeric(result):
-    global goodScore, neutralScore, badScore
-    if result == 5 or result == 4:
-        goodScore += result
-    elif result == 3:
-        neutralScore += result
-    elif result == 1 or result == 2:
-        badScore += result
-
-#def addScoreForYesNo(result):
 
 
 app.run('0.0.0.0', port=5000, debug=True)
